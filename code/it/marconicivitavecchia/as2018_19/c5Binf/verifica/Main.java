d7eb69fc1dc0088b20555e95dd575a2c9faf0066
Your Name <you@example.com>
2018-11-06 11:38:04 +0100

    Consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,6 +1,6 @@
 
 // TODO Inserire il package corretto
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
@@ -11,16 +11,65 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(600,600);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html>");
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<h1> Almanacco astronomico </h1>");
+		sb.append("<h2> Novembre 2018 </h2>");
+		sb.append("<b>Il Sole.</b> In Novembre perderemo complessivamente 1 ora e 3 minuti di luce");
+		sb.append("<table border='1'>");
+		sb.append("<tr>");
+		sb.append("<td>");
+		sb.append("<b>Data</b>");
+		sb.append("</td>");
+		sb.append("<td>");
+		sb.append("<b>Sorge</b>");
+		sb.append("</td>");
+		sb.append("<td>");
+		sb.append("<b>Tramonta</b>");
+		sb.append("</td>");
+		sb.append("<tr>");
+		sb.append("</tr>");
+		sb.append("<td>");
+		sb.append("1 gio");
+		sb.append("</td>");
+		sb.append("<td>");
 		
+		sb.append("</td>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("<tr>");
+		sb.append("</tr>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("<tr>");
+		sb.append("</tr>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("<td>");
+		
+		sb.append("</td>");
+		sb.append("</table>");
 		// TODO Convertire lo StringBuilder in String
-		
+		String htmlText = sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(htmlText));
 		super.setVisible(true);
 		 
 		System.out.println(htmlText);
